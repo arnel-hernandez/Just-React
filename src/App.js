3c187ea76d8d29177db825513e7651a155591bfd
samuel <aarnelhernandez@gmail.com>
2019-05-22 11:47:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import About from './components/folder/about'
+import Header from './components/layout/layer'
+import uuid from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        todos: [{
+                id: uuid.v4(),
+                title: 'Clean Car',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'Clean House',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'Mow the Yard',
+                completed: false
+            }
+        ]
+    }
+    // TOGGLE COMPLETE MARKING
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        })})
+    }
+
+    // DELETE TODO
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+    }
+
+    //ADD TODO
+
+    addTodo = (title) => {
+        const newTodo = {
+            id: uuid.v4(),
+            title: title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo]})
+    }
+
+    render() {
+        console.log(this.state.todos);
+        return (
+            <Router>
+            <div className = "App" >
+                <div className='container'>
+                    <Header />
+                    <Route exact path='/' render={props => (
+                        <React.Fragment>
+                            <AddTodo addTodo={this.addTodo} />
+                            <Todos todos={this.state.todos} markComplete= {this.markComplete} delTodo={this.delTodo}/>
+                        </React.Fragment>
+                    )} />
+                    <Route path='/about' component={About} />
+                </div>
+            </div >
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
+
+
+class Todos extends Component {
+
+markComplete = () => {
+    console.log('Todos');
+}
+
+    render() {
+        // console.log(this.props.todos);
+        return this.props.todos.map((todo) => (
+            <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+        ));
+    }
+}
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
